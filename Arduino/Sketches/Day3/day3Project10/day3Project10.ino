787505167d653174f5e44fa067df42ed6b6b29f6
anilkunchalaece@gmail.com <anilkunchalaece@gmail.com>
2017-02-12 17:42:41 +0530

    added dht11

---
+++ Arduino/Sketches/Day3/day3Project10/day3Project10.ino
@@ -0,0 +1,23 @@
+
+#include "DHT.h"
+#define DHTPIN 2     
+#define DHTTYPE DHT11   
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  Serial.print("Humidity: ");
+  Serial.println(h);
+
+  Serial.print("Temperature: ");
+  Serial.println(t);
+}
---
+++ Arduino/Sketches/Day3/day3Project9/day3Project9.ino
@@ -0,0 +1,17 @@
+//Author : Team LearnIt
+void setup(){
+
+  Serial.begin(9600);
+
+  Serial.print("hello ");
+  Serial.print("World ");
+
+  Serial.println("From");
+  
+  Serial.print("Arduino")
+  }
+
+  void loop(){
+    //do nothing
+  }
+
